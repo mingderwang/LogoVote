27de4fbad942251f0fdd41d52639c5ce46c824b1
Ming-der Wang <ming@log4analytics.com>
2017-06-20 11:14:00 +0800

    ok for fetch no redux

--- src/routes/Counter2/modules/counter2.js
+++ src/routes/Counter2/modules/counter2.js
@@ -1,3 +1,33 @@
+
+export function itemsHasErrored (bool) {
+  return {
+    type: 'ITEMS_HAS_ERRORED',
+    hasErrored: bool
+  }
+}
+export function itemsIsLoading (bool) {
+  return {
+    type: 'ITEMS_IS_LOADING',
+    isLoading: bool
+  }
+}
+export function itemsFetchDataSuccess (items) {
+  return {
+    type: 'ITEMS_FETCH_DATA_SUCCESS',
+    items
+  }
+}
+
+export function errorAfterFiveSeconds () {
+    // We return a function instead of an action object
+  return (dispatch) => {
+    setTimeout(() => {
+            // This function is able to dispatch other action creators
+      dispatch(itemsHasErrored(true))
+    }, 5000)
+  }
+}
+
 // ------------------------------------
 // Constants
 // ------------------------------------
