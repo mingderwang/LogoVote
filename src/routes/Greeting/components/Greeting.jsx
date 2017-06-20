4ec7b3a338907d86945c4c20476a26cd0a158261
Ming-der Wang <ming@log4analytics.com>
2017-06-20 23:54:11 +0800

    connect to metaMask vk

--- src/routes/Greeting/components/Greeting.jsx
+++ src/routes/Greeting/components/Greeting.jsx
@@ -42,18 +42,7 @@ var Greeting = createReactClass({
 })
   },
   componentDidMount () {
-// Checking if Web3 has been injected by the browser (Mist/MetaMask)
-            if (typeof web3 !== 'undefined') {
-                 // Use Mist/MetaMask's provider
-                 web3 = new Web3(web3.currentProvider);
-             } else {
-                 console.log('No web3? You should consider trying MetaMask!')
-                 // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-//                 web3 = new Web3(new Web3.providers.HttpProvider("http://pub-node1.etherscan.io:8545")); //main net
-                 // Ropsten Testnet 
-                 web3 = new Web3(new Web3.providers.HttpProvider("http://163.172.171.98:8545")); //Ropsten test net
-         }
-         web3.version.getNetwork((err, netId) => {
+         window.web3.version.getNetwork((err, netId) => {
   switch (netId) {
     case "1":
       console.log('This is mainnet')
