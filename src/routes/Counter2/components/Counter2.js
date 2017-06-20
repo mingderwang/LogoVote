6fcbe80a454575aca930c9b6836480e6706c140f
Ming-der Wang <ming@log4analytics.com>
2017-06-20 10:20:30 +0800

    test https://medium.com/@stowball/a-dummys-guide-to-redux-and-thunk-in-react-d8904a7005d3

--- src/routes/Counter2/components/Counter2.js
+++ src/routes/Counter2/components/Counter2.js
@@ -1,22 +1,49 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Counter2 = ({ counter, increment, doubleAsync }) => {
-  if (counter.hasErrored) {
-    return <p>Sorry! There was an error loading the items</p>
+class Counter2 extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      items: [
+        {
+          id: 1,
+          label: 'List item 1'
+        },
+        {
+          id: 2,
+          label: 'List item 2'
+        },
+        {
+          id: 3,
+          label: 'List item 3'
+        },
+        {
+          id: 4,
+          label: 'List item 4'
+        }
+      ],
+      hasErrored: false,
+      isLoading: false
+    }
   }
-  if (counter.isLoading) {
-    return <p>Loading…</p>
-  }
-  return (
-    <ul>
-      {counter.items.map((item) => (
-        <li key={item.id}>
-          {item.label}
-        </li>
+  render () {
+    if (this.state.hasErrored) {
+      return <p>Sorry! There was an error loading the items</p>
+    }
+    if (this.state.isLoading) {
+      return <p>Loading…</p>
+    }
+    return (
+      <ul>
+        {this.state.items.map((item) => (
+          <li key={item.id}>
+            {item.label}
+          </li>
                 ))}
-    </ul>
-  )
+      </ul>
+    )
+  }
 }
 
 Counter2.propTypes = {
