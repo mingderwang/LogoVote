65c8277242ae0af9c09a2cafafb9feca27fd4d0f
Ming-der Wang <ming@log4analytics.com>
2017-07-04 00:16:00 +0800

    add count down and endblock

--- src/routes/Greeting/components/Greeting.jsx
+++ src/routes/Greeting/components/Greeting.jsx
@@ -10,11 +10,15 @@ const abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","t
 const abiVote =
 [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"initialSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"}]
 
+const abiFaucet =
+[{"constant":false,"inputs":[],"name":"getToken","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"tokenAddress","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"_tokenAddress","type":"address"}],"payable":false,"type":"constructor"},{"payable":false,"type":"fallback"}]
+
 const ContractAddress = '0x3e2506af1b529b55b42abd8e27921929e682f61b'
 const address = '0xcd52095f9c3916681fe6e745d61c8599b393faa6'
 const VoteAddress = '0xcd52095f9c3916681fe6e745d61c8599b393faa6'
 const Logo1 = '0xd085758bfd6e5e7560d7d9e3dcc50c99f38d74e8'
 const Logo2 = '0xa7199b1011aea537447c43d0f1d8d91ec3fbf84c'
+const Faucet = '0xf8f1b111585ad074a1f3e763178a7f736cd5a421'
 function initContract (contract) {
 
 
@@ -30,7 +34,7 @@ myContractInstance.balanceOf(web3.eth.accounts[0],function(error, result) { cons
 var createReactClass = require('create-react-class')
 var Greeting22 = createReactClass({
   handleClick1 (event) {
-    var VoteContract = window.web3.eth.contract(abiVote);
+var VoteContract = window.web3.eth.contract(abiVote);
 var myVoteContractInstance = VoteContract.at(VoteAddress)
 
 myVoteContractInstance.transfer(Logo1,1, function(error, result) {
@@ -41,8 +45,22 @@ myVoteContractInstance.transfer(Logo1,1, function(error, result) {
 }
 })
   },
-  handleClick2 (event) {
-    var VoteContract = window.web3.eth.contract(abiVote);
+
+handleClickFaucet (event) {
+
+var FaucetContract = window.web3.eth.contract(abiFaucet);
+var FaucetContractInstance = FaucetContract.at(Faucet)
+FaucetContractInstance.getToken( function(error, result) {
+  if(!error) {
+  console.log(result)
+} else {
+  console.log(error)
+}
+})
+},
+
+handleClick2 (event) {
+var VoteContract = window.web3.eth.contract(abiVote);
 var myVoteContractInstance = VoteContract.at(VoteAddress)
 
 myVoteContractInstance.transfer(Logo2,1, function(error, result) {
@@ -156,11 +174,14 @@ myVoteContractInstance.balanceOf(Logo2, function(error, result) {
         <p></p>
         <h2>Your Address: </h2>
         <h4>{message}</h4>
+        <iframe src="https://www.powr.io/plugins/countdown-timer/view?unique_label=85f91fd1_1499096974&external_type=iframe" width="100%" height="200" frameborder="0"></iframe>
         <p></p>
         <h2>You have Taipei Ethereum LogoVote Token: </h2>
         <h4>{token}</h4>
         <h2> currentBlock: </h2>
         <h4>{currentBlock}</h4>
+        <h2> endBlock: </h2>
+        <h4>1295975</h4>
               <img src="https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg" alt="ET" style={{width: 400}} />
         <h2> 阿花 got: </h2>
         <h4>{logo1}</h4>
@@ -169,6 +190,8 @@ myVoteContractInstance.balanceOf(Logo2, function(error, result) {
         <h4>{logo2}</h4>
         <button onClick={this.handleClick1}>我要投 阿花 1 票</button>
         <button onClick={this.handleClick2}>我要投 ET 1 票</button>
+
+        <button onClick={this.handleClickFaucet}>我要索取一張免費選票</button>
       </div>
     )
   }
